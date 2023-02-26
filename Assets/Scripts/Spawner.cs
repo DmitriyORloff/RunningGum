d985e006485040f09261b36610b44e7c1fbeca1b
Eldar Malyshov <eldar.malyshov@gmail.com>
2023-02-26 04:10:42 +0400

    enemy spawn/annihilation behaviour fix

--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -6,20 +6,44 @@ public class Spawner : MonoBehaviour
 {
     public GameObject[] enemyVariants;
     public SpeedManager speedManager;
+    public GameObject enemyPrefab;
+
+    private readonly float YDisposition = 3.15f;
 
     public float countdown = 0;
 
     private void Update()
     {
+        countdown -= Time.deltaTime;
         if(countdown <= 0)
         {
-            int rand = Random.Range(0, enemyVariants.Length);
-            Instantiate(enemyVariants[rand], transform.position, Quaternion.identity);
+            SpawnRandom();
             countdown = 1 / speedManager.GetSpawnRate();
         }
-        else
+    }
+
+    private void SpawnRandom()
+    {
+        switch(Random.Range(0, 3))
         {
-            countdown -= Time.deltaTime;
+            case 0:
+                SpawnAt(1);
+                SpawnAt(0);
+                break;
+            case 1:
+                SpawnAt(-1);
+                SpawnAt(0);
+                break;
+            case 2:
+                SpawnAt(1);
+                SpawnAt(-1);
+                break;
         }
+
+    }
+    private void SpawnAt(int track)
+    {
+        Vector2 enemySpawnPosition = new(transform.position.x, YDisposition * track);
+        Instantiate(enemyPrefab, enemySpawnPosition, Quaternion.identity);
     }
 }
